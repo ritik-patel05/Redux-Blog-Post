b9f69dd80b44cf872fa010b751cbfcd3e8fa154f
RitikPPatel05 <ritikpatel05121999@gmail.com>
2021-03-01 20:20:08 +0530

    Modified Router code to new style | Converted to arrow based

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import { Navbar } from './app/Navbar'
 
 import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Navbar />
       <div className="App">
         <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <React.Fragment>
-                <AddPostForm />
-                <PostsList />
-              </React.Fragment>
-            )}
-          />
-          <Redirect to="/" />
+          <Route exact path="/">
+            <>
+              <AddPostForm />
+              <PostsList />
+            </>
+          </Route>
         </Switch>
       </div>
     </Router>
