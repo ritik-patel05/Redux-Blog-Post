41dc45cb2d7315387236b916803647e6579c6fcd
RitikPPatel05 <ritikpatel05121999@gmail.com>
2021-03-01 19:27:51 +0530

    Added Posts components and basic setup

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import { Navbar } from './app/Navbar'
+
+import { PostsList } from './features/posts/PostsList'
+import { AddPostForm } from './features/posts/AddPostForm'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <AddPostForm />
+                <PostsList />
+              </React.Fragment>
+            )}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
