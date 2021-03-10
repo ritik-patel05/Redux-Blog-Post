ef402c74fbf1333e6a378f504e5a8226b1ed2fd3
RitikPPatel05 <ritikpatel05121999@gmail.com>
2021-03-10 22:38:15 +0530

    Add notifications feature

---
+++ src/features/notifications/notificationsSlice.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+import { client } from '../../api/client'
+
+export const fetchNotifications = createAsyncThunk(
+  'notifications/fetchNotifications',
+  async (_, { getState }) => {
+    const allNotifications = selectAllNotifications(getState())
+    const [latestNotification] = allNotifications
+    const latestTimestamp = latestNotification ? latestNotification.date : ''
+    const response = await client.get(
+      `/fakeApi/notifications?since=${latestTimestamp}`
+    )
+    return response.notifications
+  }
+)
+
+const notificationsSlice = createSlice({
+  name: 'notifications',
+  initialState: [],
+  reducers: {
+    allNotificationsRead(state, action) {
+      state.forEach((notification) => {
+        notification.read = true
+      })
+    },
+  },
+  extraReducers: {
+    [fetchNotifications.fulfilled]: (state, action) => {
+      state.forEach((notification) => {
+        notification.isNew = !notification.read
+      })
+      state.push(...action.payload)
+      // sort with newest first
+      state.sort((a, b) => b.date.localeCompare(a.date))
+    },
+  },
+})
+
+export const { allNotificationsRead } = notificationsSlice.actions
+
+export default notificationsSlice.reducer
+
+export const selectAllNotifications = (state) => state.notifications
